beedc848395a62f20b010e930e33f831e7f9dca0
HyperbolicHippo <splishsplashno@gmail.com>
2022-11-28 22:28:59 +0000

    Added Fabric mod files

---
+++ src/main/java/hyperbolichippo/techadvancements/TechnologicalAdvancementsClient.java
@@ -0,0 +1,12 @@
+package hyperbolichippo.techadvancements;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TechnologicalAdvancementsClient implements ClientModInitializer {
+
+	@Override
+	public void onInitializeClient() {
+
+	}
+
+}
